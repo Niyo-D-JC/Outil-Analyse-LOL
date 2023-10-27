4b581598245bb71c0330045bc4e1be993d86ffba
Max2321 <maxime.tio@eleve.ensai.fr>
2023-10-27 16:56:41 +0200

    ajout d'un commentaire

--- src/view/creer_compte_vue.py
+++ src/view/creer_compte_vue.py
@@ -4,7 +4,8 @@ from view.vue_abstraite import VueAbstraite
 from services.joueur_service import JoueurService
 from business_object.user.user import User
 from services.user_service import UserService
-import time 
+import time
+
 
 class CreerCompteVue(VueAbstraite):
     def __init__(self, message=""):
@@ -12,7 +13,11 @@ class CreerCompteVue(VueAbstraite):
         self.questions = [
             {"type": "input", "name": "pseudo", "message": "Entrez votre username :"},
             {"type": "input", "name": "mdp", "message": "Entrez votre mot de passe :"},
-            {"type": "input", "name": "joueur", "message": "Nom de votre Joueur (Optionel) :"},
+            {
+                "type": "input",
+                "name": "joueur",
+                "message": "Nom de votre Joueur (Optionel) :",
+            },
         ]
 
     def afficher(self):
@@ -22,29 +27,36 @@ class CreerCompteVue(VueAbstraite):
 
     def choisir_menu(self):
         answers = prompt(self.questions)
-        
+
         puuid = ""
         user = None
-        
-        joueur = JoueurService().find_by_name(answers["pseudo"])
-        if joueur != None :
-            user = User(answers["pseudo"],answers["mdp"],"User",joueur)
+
+        joueur = JoueurService().find_by_name(
+            answers["pseudo"]
+        )  # il faudrait faire un requete API au lieu de chercher dans la BDD
+
+        if joueur != None:
+            user = User(answers["pseudo"], answers["mdp"], "User", joueur)
             UserService().creer(user)
-        else :    
-            print("")         
+        else:
+            print("")
             print("Aucun Joueur Referencé")
-            user = User(answers["pseudo"],answers["mdp"],"User")    
+            user = User(answers["pseudo"], answers["mdp"], "User")
             UserService().creer_no_puuid(user)
-        
+
         print("")
-        print("-------------------------Votre Compte a été Créé-------------------------")
+        print(
+            "-------------------------Votre Compte a été Créé-------------------------"
+        )
         print("")
-        print("-----------------------Retour à la page d'accueil-------------------------")
+        print(
+            "-----------------------Retour à la page d'accueil-------------------------"
+        )
         time.sleep(3)
         from view.accueil_vue import AccueilVue
-        return AccueilVue("Bienvenue sur Votre Application ViewerOn LoL") 
 
-       
+        return AccueilVue("Bienvenue sur Votre Application ViewerOn LoL")
+
 
 if __name__ == "__main__":
-     CreerCompteVue("").choisir_menu()
+    CreerCompteVue("").choisir_menu()
