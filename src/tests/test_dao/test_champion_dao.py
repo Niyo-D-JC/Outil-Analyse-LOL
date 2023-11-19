b1dfb09c94617c2c42e70af5a7de562f9e0306f7
Diego <diegomrtnz83@gmail.com>
2023-11-19 12:19:30 +0100

    test_champion_dao

--- src/tests/test_dao/test_champion_dao.py
+++ src/tests/test_dao/test_champion_dao.py
@@ -1,65 +1,44 @@
 import unittest
 from unittest.mock import patch, MagicMock
+import os
+import sys
+champion_dao_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'src', 'dao'))
+sys.path.insert(0, champion_dao_path)
 from dao.champion_dao import ChampionDao
+from business_object.tools.champion import Champion
 
 class TestChampionDao(unittest.TestCase):
+    def setUp(self):
+        self.mock_champion = Champion(champion_id=1, name="TestChampion")
 
-    @patch('dao.db_connection.DBConnection')
-    def test_creer_method(self, mock_db_connection):
+    @patch('dao.champion_dao.DBConnection')
+    def test_creer_champion(self, mock_db_connection):
         champion_dao = ChampionDao()
-
-        # Création d'un champion factice pour les tests
-        fake_champion = MagicMock()
-        fake_champion.tools_id = 1
-        fake_champion.name = "Fake Champion"
-
-        # Définition du comportement simulé de la connexion à la base de données
-        mock_db_instance = mock_db_connection.return_value
-        mock_db_instance.connection.__enter__.return_value.cursor.return_value.execute.return_value = None
-
-        # Appel de la méthode à tester
-        result = champion_dao.creer(fake_champion)
-
-        # Vérification que la méthode renvoie True pour la création réussie
+        mock_cursor = MagicMock()
+        mock_cursor.__enter__.return_value.execute.return_value = None
+        mock_db_connection.return_value.connection.__enter__.return_value.cursor.return_value = mock_cursor
+        result = champion_dao.creer(self.mock_champion)
         self.assertTrue(result)
 
-    @patch('dao.db_connection.DBConnection')
-    def test_find_by_id_method(self, mock_db_connection):
+    @patch('dao.champion_dao.DBConnection')
+    def test_find_by_id(self, mock_db_connection):
         champion_dao = ChampionDao()
-
-        # Définition d'un champion factice pour les tests
-        champion_id = 1
         mock_cursor = MagicMock()
-        mock_cursor.fetchone.return_value = {"name": "Fake Champion"}
+        mock_cursor.__enter__.return_value.fetchone.return_value = {"name": "TestChampion"}
+        mock_db_connection.return_value.connection.__enter__.return_value.cursor.return_value = mock_cursor
+        champion = champion_dao.find_by_id(1)
+        self.assertIsNotNone(champion)
+        self.assertEqual(champion.name, "TestChampion")
 
-        # Définition du comportement simulé de la connexion à la base de données
-        mock_db_instance = mock_db_connection.return_value
-        mock_db_instance.connection.__enter__.return_value.cursor.return_value.execute.return_value = mock_cursor
-
-        # Appel de la méthode à tester
-        result = champion_dao.find_by_id(champion_id)
-
-        # Vérification que la méthode renvoie un objet Champion
-        self.assertIsNotNone(result)
-        self.assertEqual(result.name, "Fake Champion")
-
-    @patch('dao.db_connection.DBConnection')
-    def test_get_all_order_method(self, mock_db_connection):
+    @patch('dao.champion_dao.DBConnection')
+    def test_get_all_order(self, mock_db_connection):
         champion_dao = ChampionDao()
-
-        # Définition d'un jeu de résultats factice pour les tests
         mock_cursor = MagicMock()
-        mock_cursor.fetchall.return_value = [("Champion1", 10, 0.7), ("Champion2", 20, 0.6)]
-
-        # Définition du comportement simulé de la connexion à la base de données
-        mock_db_instance = mock_db_connection.return_value
-        mock_db_instance.connection.__enter__.return_value.cursor.return_value.execute.return_value = mock_cursor
-
-        # Appel de la méthode à tester
+        mock_cursor.__enter__.return_value.fetchall.return_value = [("Champion1", 10, 0.6), ("Champion2", 5, 0.4)]
+        mock_db_connection.return_value.connection.__enter__.return_value.cursor.return_value = mock_cursor
         result = champion_dao.get_all_order()
-
-        # Vérification que la méthode renvoie un jeu de résultats non vide
         self.assertIsNotNone(result)
+        self.assertIsInstance(result, list)
         self.assertEqual(len(result), 2)
 
 if __name__ == '__main__':
