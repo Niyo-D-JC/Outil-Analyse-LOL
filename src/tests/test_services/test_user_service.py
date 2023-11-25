0c37a379e7a8d72b83ead09a29a90f148b2af9ef
Max2321 <maxime.tio@eleve.ensai.fr>
2023-11-25 14:40:39 +0100

    fin test

--- src/tests/test_services/test_user_service.py
+++ src/tests/test_services/test_user_service.py
@@ -5,100 +5,139 @@ from services.user_service import UserService
 
 from business_object.battle.matchjoueur import MatchJoueur
 from business_object.stats.stat_joueur import StatJoueur
+from business_object.tools.champion import Champion
+from business_object.tools.lane import Lane
 
 
 class TestUserService(TestCase):
-    def test_get_coef_damage_type(self):
+    def test_get_global_WR(self):
         # GIVEN
-        liste_matchs = [
-            MatchJoueur(
-                1,
-                "Joueur1",
-                "Champion1",
-                ["Item1", "Item2"],
-                "Top",
-                "Team1",
-                StatJoueur(5, 2, 3, 100, 20, 50, 4.0, True),
+
+        Match_Object1 = MatchJoueur(
+            match_id=None,
+            joueur=None,
+            champion=Champion(1, "Annie"),
+            items=None,
+            lane=Lane(3, "MIDDLE"),
+            team=None,
+            stat_joueur=StatJoueur(
+                total_damage_dealt=2000,
+                total_damage_take=1500,
+                total_heal=800,
+                kills=10,
+                deaths=2,
+                assists=5,
+                creeps=200,
+                total_gold=10000,
+                win=True,
             ),
-            MatchJoueur(
-                2,
-                "Joueur1",
-                "Champion1",
-                ["Item1", "Item2"],
-                "Top",
-                "Team1",
-                StatJoueur(8, 3, 5, 120, 25, 60, 4.0, True),
+        )
+
+        Match_Object2 = MatchJoueur(
+            match_id=None,
+            joueur=None,
+            champion=Champion(42, "Corki"),
+            items=None,
+            lane=Lane(3, "MIDDLE"),
+            team=None,
+            stat_joueur=StatJoueur(
+                total_damage_dealt=2000,
+                total_damage_take=1500,
+                total_heal=800,
+                kills=10,
+                deaths=2,
+                assists=5,
+                creeps=200,
+                total_gold=10000,
+                win=False,
             ),
-            MatchJoueur(
-                3,
-                "Joueur1",
-                "Champion1",
-                ["Item1", "Item2"],
-                "Top",
-                "Team1",
-                StatJoueur(3, 1, 2, 80, 15, 40, 6.0, False),
-            ),
-            # ... Ajoutez d'autres matchs selon vos besoins
-        ]
+        )
+
+        Liste_Match_User = [Match_Object1, Match_Object2]
 
         # WHEN
-        total_wins, total_games = UserService().get_global_WR(liste_matchs)
+        total_wins, total_games = UserService().get_global_WR(Liste_Match_User)
 
         # THEN
-        self.assertEqual(total_wins, 2)
-        self.assertEqual(total_games, 3)
+        self.assertEqual(total_wins, 1)
+        self.assertEqual(total_games, 2)
 
-    def test_get_stats_by_champ(self):
+    def test_get_stats_by_lane(self):
         # GIVEN
-        liste_matchs = [
-            MatchJoueur(
-                1,
-                "Joueur1",
-                "Champion1",
-                ["Item1", "Item2"],
-                "Top",
-                "Team1",
-                StatJoueur(5, 2, 3, 100, 20, 50, 4.0, True),
+        Match_Object1 = MatchJoueur(
+            match_id=None,
+            joueur=None,
+            champion=Champion(1, "Annie"),
+            items=None,
+            lane=Lane(3, "MIDDLE"),
+            team=None,
+            stat_joueur=StatJoueur(
+                total_damage_dealt=2000,
+                total_damage_take=1500,
+                total_heal=800,
+                kills=10,
+                deaths=5,
+                assists=3,
+                creeps=200,
+                total_gold=10000,
+                win=True,
             ),
-            MatchJoueur(
-                2,
-                "Joueur1",
-                "Champion1",
-                ["Item1", "Item2"],
-                "Top",
-                "Team1",
-                StatJoueur(8, 3, 5, 120, 25, 60, 4.0, True),
+        )
+
+        Match_Object2 = MatchJoueur(
+            match_id=None,
+            joueur=None,
+            champion=Champion(1, "Annie"),
+            items=None,
+            lane=Lane(3, "MIDDLE"),
+            team=None,
+            stat_joueur=StatJoueur(
+                total_damage_dealt=2000,
+                total_damage_take=1500,
+                total_heal=800,
+                kills=3,
+                deaths=2,
+                assists=9,
+                creeps=160,
+                total_gold=10000,
+                win=True,
             ),
-            MatchJoueur(
-                3,
-                "Joueur2",
-                "Champion2",
-                ["Item3", "Item4"],
-                "Mid",
-                "Team2",
-                StatJoueur(3, 1, 2, 80, 15, 40, 6.0, False),
+        )
+
+        Match_Object3 = MatchJoueur(
+            match_id=None,
+            joueur=None,
+            champion=Champion(1, "Annie"),
+            items=None,
+            lane=Lane(3, "MIDDLE"),
+            team=None,
+            stat_joueur=StatJoueur(
+                total_damage_dealt=1800,
+                total_damage_take=1200,
+                total_heal=700,
+                kills=2,
+                deaths=8,
+                assists=6,
+                creeps=180,
+                total_gold=9500,
+                win=False,
             ),
-            # ... Ajoutez d'autres matchs selon vos besoins
-        ]
+        )
+
+        Liste_Match_User = [Match_Object1, Match_Object2, Match_Object3]
 
         # WHEN
-        champions_stats = UserService().get_stats_by_champ(liste_matchs)
+        champions_stats = UserService().get_stats_by_lane(Liste_Match_User)
 
         # THEN
-        # Vérifiez les statistiques pour chaque champion
-        self.assertEqual(champions_stats["Champion1"]["kills_avg"], 6.5)
-        self.assertEqual(champions_stats["Champion1"]["deaths_avg"], 2.5)
-        self.assertEqual(champions_stats["Champion1"]["assists_avg"], 4.0)
-        self.assertEqual(champions_stats["Champion1"]["cs_avg"], 55.0)
-        self.assertEqual(champions_stats["Champion1"]["nombre_de_matchs"], 2)
-
-        self.assertEqual(champions_stats["Champion2"]["kills_avg"], 3.0)
-        self.assertEqual(champions_stats["Champion2"]["deaths_avg"], 1.0)
-        self.assertEqual(champions_stats["Champion2"]["assists_avg"], 2.0)
-        self.assertEqual(champions_stats["Champion2"]["cs_avg"], 40.0)
-        self.assertEqual(champions_stats["Champion2"]["nombre_de_matchs"], 1)
-
-        # Ajoutez des assertions supplémentaires si nécessaire
+        self.assertEqual(champions_stats["MIDDLE"]["Annie"]["kills_avg"], 5.0)
+        self.assertEqual(champions_stats["MIDDLE"]["Annie"]["deaths_avg"], 5.0)
+        self.assertEqual(champions_stats["MIDDLE"]["Annie"]["assists_avg"], 6.0)
+        self.assertEqual(champions_stats["MIDDLE"]["Annie"]["cs_avg"], 180.0)
+        self.assertEqual(
+            champions_stats["MIDDLE"]["Annie"]["winrate"], 0.6666666666666666
+        )
+        self.assertEqual(champions_stats["MIDDLE"]["Annie"]["nombre_de_matchs"], 3)
 
 
 if __name__ == "__main__":
